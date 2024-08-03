fba2a1ee4edb9fc11e9659bf65633060aded14d5
Abdullah Amr Sobh <abdullahamrsobh@gmail.com>
2024-08-03 23:11:22 +0300

    feat: Add smart unique ptr type

---
+++ TL/Include/TL/UniquePtr.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include <TL/Containers.hpp>
+
+namespace TL
+{
+    // @todo: should rename to UniquePtr?
+
+    // @todo: expand this (should not be just an alias)
+
+    template<typename T>
+    using Ptr = std::unique_ptr<T>;
+
+    template<class T, class... Args>
+    inline constexpr Ptr<T> CreatePtr(Args... args)
+    {
+        return std::make_unique<T, Args...>(std::forward<Args>(args)...);
+    }
+
+    template<typename T, typename U, typename... Args>
+    inline constexpr T* EmplacePtr(TL::Vector<Ptr<U>>& container, Args... args)
+    {
+        return (T*)container.emplace_back(CreatePtr<T>(args...)).get();
+    }
+} // namespace TL
