40d517a3b6c7ae15c706b34b9dfbd0b4c7f6490d
Abdullah Amr Sobh <abdullahamrsobh@gmail.com>
2025-08-23 18:47:14 +0300

    Add allocator concept

--- TL/Include/TL/Allocator/Allocator.hpp
+++ TL/Include/TL/Allocator/Allocator.hpp
@@ -7,6 +7,16 @@ namespace TL
 {
     /// Reading: (https://www.foonathan.net/2022/08/malloc-interface/)
 
+    template<typename T>
+    concept CAllocator = requires(T a, size_t size, size_t alignment, Block block, size_t newSize)
+    {
+        { a.Allocate(size, alignment) } -> std::same_as<Block>;
+        { a.Reallocate(block, newSize, alignment) } -> std::same_as<Block>;
+        { a.Release(block, alignment) } -> std::same_as<void>;
+        { a == a } -> std::same_as<bool>;
+        { a != a } -> std::same_as<bool>;
+    };
+
     // Polymorphic allocator interface used by TL
     class TL_EXPORT IAllocator
     {
@@ -37,8 +47,8 @@ namespace TL
         virtual bool operator!=(TL_MAYBE_UNUSED const IAllocator& other) const noexcept { return false; }
 
     protected:
-        virtual Block AllocateImpl(size_t size, size_t alignment)   = 0;
+        virtual Block AllocateImpl(size_t size, size_t alignment)                   = 0;
         virtual Block ReallocateImpl(Block block, size_t newSize, size_t alignment) = 0;
-        virtual void  ReleaseImpl(Block block, size_t alignment)    = 0;
+        virtual void  ReleaseImpl(Block block, size_t alignment)                    = 0;
     };
 } // namespace TL
